00e2b22b5b01591c0f0736d9240b5e93789fa211
kingstar <athenesaint@gmail.com>
2023-06-29 22:58:24 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import openai
+from difflib import get_close_matches
+from tkinter import Tk, Entry, Button, Text, Scrollbar
+
+openai.api_key = 'GPT_API_KEY'  # Replace with your OpenAI API key
+
+class Chatbot:
+    def __init__(self, window):
+        window.title('Bot Assistant')
+        window.geometry('405x400')
+        window.resizable(0, 0)
+        self.message_session = Text(window, bd=3, relief="flat", font=(
+            "Times", 10), undo=True, wrap="word")
+        self.message_session.config(
+            width=55, height=15, bg="gray", fg="white", state='disabled')
+        self.overscroll = Scrollbar(window, command=self.message_session.yview)
+        self.overscroll.config(width=20)
+        self.message_session["yscrollcommand"] = self.overscroll.set
+        self.message_position = 1.5
+        self.send_button = Button(window, text='send', fg='white', bg='blue', width=9, font=(
+            'Times', 12), relief='flat', command=self.reply_to_you)
+        self.Message_Entry = Entry(window, width=40, font=('Times', 12))
+        self.Message_Entry.bind('<Return>', self.reply_to_you)
+        self.message_session.place(x=20, y=20)
+        self.overscroll.place(x=370, y=50)
+        self.send_button.place(x=140, y=290)
+        self.Message_Entry.place(x=30, y=260)
+        self.Brain = {
+            'hello': ['Hello!', 'Hi there!', 'Greetings!'],
+            'how are you': ["I'm good. How about you?", "I'm fine, thank you.", "I'm great!"],
+            'bye': ['Goodbye!', 'See you later!', 'Take care!']
+        }
+
+    def add_chat(self, message):
+        self.message_position += 1.5
+        print(self.message_position)
+        self.Message_Entry.delete(0, 'end')
+        self.message_session.config(state='normal')
+        self.message_session.insert(self.message_position, message)
+        self.message_session.see('end')
+        self.message_session.config(state='disabled')
+
+    def reply_to_you(self, event=None):
+        message = self.Message_Entry.get().lower()
+        message = 'you: ' + message+'\n'
+        prompt = message + 'Bot:'
+
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=50,
+            n=1,
+            stop=None
+        )
+
+        reply = 'Bot: ' + response.choices[0].text.strip() + '\n'
+
+        self.add_chat(message)
+        self.add_chat(reply)
+
+
+root = Tk()
+Chatbot(root)
+root.mainloop()
